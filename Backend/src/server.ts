57d54cc54db2f4ee1a342e80ca313225ee4a3a48
Kevin Gomez <gomezk901@gmail.com>
2025-10-26 09:44:40 -0500

    Conexion del Backend con el Frontend, solucionando problemas de CORS

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import cors from "cors";
 import jwtRoutes from "./routes/jwtRoutes";
 import { conectarDB } from "./configuracion/db";
 
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
+
 
 conectarDB();
 
