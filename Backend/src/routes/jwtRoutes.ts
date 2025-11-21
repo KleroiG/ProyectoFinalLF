091f5b5ce29b785d34c16637052973356a0b11f3
Kevin Gomez <gomezk901@gmail.com>
2025-11-21 16:19:27 -0500

    Solucion error en la codificación de JWT

--- Backend/src/routes/jwtRoutes.ts
+++ Backend/src/routes/jwtRoutes.ts
@@ -161,7 +161,7 @@ router.post("/Codificar", async (req, res) => {
   // Si hubo error en alguna fase
   if (!result.token) {
     await TokenResult.create({
-      token: null,
+      token: result,
       tipo: "Codificación Fallida",
       algoritmo: header?.alg || "No disponible",
       detalles: result,
